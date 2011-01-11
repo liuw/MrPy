19cd37b5ef0ffa95e9f80dd452b992111a9c54d9
liuw <liuw@liuw.name>
2011-01-11 22:13:54 +0800

    Add MASTER_MPORT, which can be used by master to receive messages from slaves.

--- common/Config.py
+++ common/Config.py
@@ -2,9 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # Slave use this port to receive commands
-SLAVE_CPORT = 9000
+SLAVE_CPORT = 9001
 # Slave use this port to send data (via httpd)
-SLAVE_DPORT = 9001
+SLAVE_DPORT = 9002
+
+# Master use this port to receive message from slaves
+MASTER_MPORT = 9000
 
 # Global FS, can be NFS or anything else
 SHARED_STORAGE = "/path/to/shared/storage"
